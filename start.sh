c1aaca7809ef0a74c35bc9223f52224435619645
Ronei Kunkel <ronei.kunkel@gmail.com>
2023-11-10 20:11:39 -0300

    Fix Bugs

--- start.sh
+++ start.sh
@@ -2,14 +2,12 @@
 
 ### Network
 
-NETWORK_NAME="ish_internal"
-
-if [ -z "$(docker network ls -q -f name=$NETWORK_NAME)" ]; then
-    docker network create -d bridge $NETWORK_NAME
-    echo "Rede '$NETWORK_NAME' criada com sucesso"
+if [ -z "$(docker network ls -q -f name=ish_internal)" ]; then
+    docker network create -d bridge ish_internal
+    echo "Rede 'ish_internal' criada com sucesso"
     echo ""
 else
-    echo "A rede '$NETWORK_NAME' já existe. Ignorando a criação."
+    echo "A rede 'ish_internal' já existe. Ignorando a criação."
     echo ""
 fi
 
@@ -25,17 +23,7 @@ if [ "$(docker ps -q -f name=ish_mariadb)" ]; then
 else
     echo "O container 'ish_mariadb' será iniciado"
 
-    env $(cat .env) \
-    docker run --rm -d \
-    -v $(pwd)/.docker/database/:/var/lib/mysql/ \
-    -p 3306:3306 \
-    --name ish_mariadb \
-    --network $NETWORK_NAME \
-    -e MYSQL_ROOT_PASSWORD=$DB_PASSWORD \
-    -e MARIADB_ROOT_PASSWORD=$DB_PASSWORD \
-    -e MARIADB_PASSWORD=$DB_PASSWORD \
-    -e MARIADB_USER=$DB_USERNAME \
-    mariadb:11.0.2
+    env $(cat .env) docker run --rm -d -v $(pwd)/.docker/database/:/var/lib/mysql/ -p 3306:3306 --name ish_mariadb --network ish_internal -e MYSQL_ROOT_PASSWORD=$DB_PASSWORD -e MARIADB_ROOT_PASSWORD=$DB_PASSWORD -e MARIADB_PASSWORD=$DB_PASSWORD -e MARIADB_USER=$DB_USERNAME mariadb:11.0.2
 
     echo ""
 fi
@@ -60,19 +48,7 @@ else
     rm -rf $(pwd)/.docker/queue/.erlang.cookie
     rm -rf $(pwd)/.docker/logs/rabbitmq/rabbit.log
 
-    env $(cat .env) \
-    docker run --rm -d \
-    -v $(pwd)/.docker/queue:/var/lib/rabbitmq \
-    -v $(pwd)/.docker/rabbitmq/10-defaults.conf:/etc/rabbitmq/conf.d/10-defaults.conf \
-    -v $(pwd)/.docker/logs/rabbitmq:/var/log/rabbitmq/ \
-    -p 5672:5672 \
-    -p 15672:15672 \
-    --name ish_rabbitmq \
-    --network $NETWORK_NAME \
-    -e RABBITMQ_ERLANG_COOKIE=$Q_COOKIE \
-    -e RABBITMQ_DEFAULT_USER=$Q_USER \
-    -e RABBITMQ_DEFAULT_PASS=$Q_PASS \
-    rabbitmq:management
+    env $(cat .env) docker run --rm -d -v $(pwd)/.docker/queue:/var/lib/rabbitmq -v $(pwd)/.docker/rabbitmq/10-defaults.conf:/etc/rabbitmq/conf.d/10-defaults.conf -v $(pwd)/.docker/logs/rabbitmq:/var/log/rabbitmq/ -p 5672:5672 -p 15672:15672 --name ish_rabbitmq --network ish_internal -e RABBITMQ_ERLANG_COOKIE=$Q_COOKIE -e RABBITMQ_DEFAULT_USER=$Q_USER -e RABBITMQ_DEFAULT_PASS=$Q_PASS rabbitmq:management
 
     echo ""
 fi
@@ -95,13 +71,7 @@ if [ "$(docker ps -q -f name=ish_php82)" ]; then
 else
     echo "O container 'ish_php82' será iniciado"
 
-    docker run --rm -d \
-    -v $(pwd)/backend:/backend \
-    -w / \
-    -p 9000:9000 \
-    --name ish_php82 \
-    --network $NETWORK_NAME \
-    ish_php82
+    docker run --rm -d -v $(pwd)/backend:/backend -w / -p 9000:9000 --name ish_php82 --network ish_internal ish_php82
 
     echo ""
 fi
@@ -118,14 +88,7 @@ if [ "$(docker ps -q -f name=ish_nginx)" ]; then
 else
     echo "O container 'ish_nginx' será iniciado"
 
-    docker run --rm -d \
-    -v $(pwd)/.docker/nginx/nginx.conf:/etc/nginx/nginx.conf \
-    -v $(pwd)/.docker/logs/nginx/:/var/log/nginx/ \
-    -v $(pwd)/backend:/backend \
-    -p 80:80 \
-    --name ish_nginx \
-    --network $NETWORK_NAME \
-    nginx:stable-alpine3.17-slim
+    docker run --rm -d -v $(pwd)/.docker/nginx/nginx.conf:/etc/nginx/nginx.conf -v $(pwd)/.docker/logs/nginx/:/var/log/nginx/ -v $(pwd)/backend:/backend -p 80:80 --name ish_nginx --network ish_internal nginx:stable-alpine3.17-slim
 
     echo ""
 fi
